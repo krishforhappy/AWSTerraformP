fcc72b19e3dbd9f5c86e7b5bd61426dbd1040399
krish <krishforhappy@gmail.com>
2026-06-15 20:20:33 +0530

    added count

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "example" {
+  count = 10
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
