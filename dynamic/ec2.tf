017326fe399e9eba52fe31c29ea642bae6ddd5bb
krish <krishforhappy@gmail.com>
2026-06-17 20:51:33 +0530

    working on dynamic

--- dynamic/ec2.tf
+++ dynamic/ec2.tf
@@ -1,12 +1,11 @@
 resource "aws_instance" "example" {
-  count = 10
-  ami                    = var.ami
-  instance_type          = var.instance_type
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
-    Name        = "${var.instances[count.index]}_prod"
-    Project     = "roboshop"
+    Name = "terraform-dynamic-demo"
+    Project = "roboshop"
   }
 }
 
@@ -22,13 +21,23 @@ resource "aws_security_group" "allow_tls" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  ingress {
+dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  /* ingress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
+  } */
 
   tags = {
     Name = "allow-all-terraform"
