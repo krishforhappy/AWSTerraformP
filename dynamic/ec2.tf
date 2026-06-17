b8e7ff3011c856d31ea05695fca8431915b8c22e
krish <krishforhappy@gmail.com>
2026-06-17 22:26:32 +0530

    modified dynamic

--- dynamic/ec2.tf
+++ dynamic/ec2.tf
@@ -13,13 +13,13 @@ resource "aws_security_group" "allow_tls" {
   name        = "allow-all-terraform-change" # this is for AWS account
   description = "Allow TLS inbound traffic and all outbound traffic"
 
-  egress {
+  /* egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
+  } */
 
 dynamic "ingress" {
     for_each = var.ingress_rules
--- dynamic/variable.tf
+++ dynamic/variable.tf
@@ -1,21 +1,8 @@
 variable "ingress_rules" {
   default = [
-    {
-        port = 22
-        cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-        port = 443
-        cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-        port = 3389
-        cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-        port = 3306
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
+    { port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { port = 3389, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { port = 3306, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
   ]
 }
